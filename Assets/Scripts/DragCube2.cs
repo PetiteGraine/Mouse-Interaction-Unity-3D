1a7113c6774cbaf6d2ec7730dd96b76dee022502
Petite Graine <remi93300@hotmail.fr>
2025-04-16 10:12:45 +0200

    feat: use gravity for cube2

--- Assets/Scripts/DragCube2.cs
+++ Assets/Scripts/DragCube2.cs
@@ -6,25 +6,30 @@ public class DragCube2 : MonoBehaviour
     private float _zCoord;
     private Rigidbody _rb;
 
-    void Start()
+    private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _rb.useGravity = false;
     }
 
-    void OnMouseDown()
+    private void OnMouseDown()
     {
+        _rb.useGravity = false;
         _zCoord = Camera.main.WorldToScreenPoint(transform.position).z;
         _offset = transform.position - GetMouseWorldPosition();
     }
 
-    void OnMouseDrag()
+    private void OnMouseDrag()
     {
         Vector3 targetPos = GetMouseWorldPosition() + _offset;
         _rb.MovePosition(targetPos);
     }
 
-    Vector3 GetMouseWorldPosition()
+    private void OnMouseUp()
+    {
+        _rb.useGravity = true;
+    }
+
+    private Vector3 GetMouseWorldPosition()
     {
         Vector3 mousePoint = Input.mousePosition;
         mousePoint.z = _zCoord;
